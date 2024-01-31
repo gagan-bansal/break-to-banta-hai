5fa5ba3ea1831a68daa0ae9c3583b314c476e2b0
Gagan Bansal <gaganbansal123@gmail.com>
2024-01-31 09:20:50 +0530

    imp: replaced ascii-art with cfont

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var art = require('ascii-art');
-var actions = [
+const actions = [
 	'Move eyes left and right.',
 	'Move eyes up and down.',
 	'Move eyes diagonally',
@@ -8,6 +7,22 @@ var actions = [
 	'Close tightly your eyes',
   'Palm your eyes',
   'Focus on moving pencil',
-	'Look far through window']
-art.font(actions[Math.floor(Math.random()*9)],
-	'Doom', function(err, t) {console.log(t)});
+	'Look far through window'
+];
+const cfonts = require('cfonts');
+const action = actions[Math.floor(Math.random() * actions.length)];
+
+cfonts.say(action, {
+	font: 'block',              // define the font face
+	align: 'left',              // define text alignment
+	colors: ['system'],         // define all colors
+	background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+	letterSpacing: 1,           // define letter spacing
+	lineHeight: 1,              // define the line height
+	space: true,                // define if the output text should have empty lines on top and on the bottom
+	maxLength: '0',             // define how many character can be on one line
+	gradient: false,            // define your two gradient colors
+	independentGradient: false, // define if you want to recalculate the gradient for each new line
+	transitionGradient: false,  // define if this is a transition between colors directly
+	env: 'node'                 // define the environment cfonts is being executed in
+})
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 # first two lines are required if you are using node with nvm
-export NVM_DIR="/home/gaganb/.nvm" # set your nvm dir path
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-node /home/gaganb/break-to-banta-hai/index.js
+# export NVM_DIR="/home/gaganb/.nvm" # set your nvm dir path
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# or give direct path
+/home/gaganb/.nvm/versions/node/v18.17.1/bin/node /home/gaganb/break-to-banta-hai/index.js
 sleep 9s
